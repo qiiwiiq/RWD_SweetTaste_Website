5e3e54f7b9c88fd3aba7309a7e334f891bd9a2b5
PTT <qiiwiiq@hotmail.com>
2019-03-25 21:24:52 +0800

    Fix navigation bug

--- index.js
+++ index.js
@@ -24,11 +24,25 @@ $(document).ready(function(){
         $('.nav__link').slideToggle(500);
         $('.js--nav-icon i').toggleClass('ion-md-menu');
         $('.js--nav-icon i').toggleClass('ion-md-close-circle-outline');
-        
+
         if($('.js--nav-icon i').hasClass('ion-md-close-circle-outline')){
-            $('.nav').animate({paddingBottom: "6vw"},500);
+            $('.js--nav').animate({paddingBottom: "6vw"},500);
         } else {
-            $('.nav').animate({paddingBottom: "2vw"},500);
+            $('.js--nav').animate({paddingBottom: "1vw"},500);
+        }
+    });
+    
+    $(window).resize(function(){
+        if($(document).width()>768){
+            // 桌機模式
+            $('.js--nav__link').css("display", "block");   // 手機模式按 menu 會讓 link 隱藏，桌機模式讓他變成block
+            $('.js--nav').css("paddingBottom", "0");       // 手機模式按 menu 會讓 padding-bottom 變成 1vw，桌機模式回復 0vw
+            $('.js--nav-icon i').addClass('ion-md-menu');  // 手機模式 default
+            $('.js--nav-icon i').removeClass('ion-md-close-circle-outline');
+        } else if($('.js--nav-icon i').hasClass('ion-md-menu')){
+            // 手機模式 default
+            $('.js--nav__link').css("display", "none");     
+            $('.js--nav').css("paddingBottom", "1vw");
         }
     });
 
